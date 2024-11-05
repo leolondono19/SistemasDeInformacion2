c76b86b812df57cec6ba2b543b2552fb774d23d2
HusKTsu <juan941188@gmail.com>
2024-11-05 17:43:49 -0400

    avance de la paginacion

--- Sistemas ed informacion 2/app/views/content/home-view.php
+++ Sistemas ed informacion 2/app/views/content/home-view.php
@@ -197,30 +197,7 @@
         </div>
     </div>
 
-    <!-- Paginación -->
-    <nav aria-label="Page navigation">
-        <ul class="pagination">
-            <?php if ($pagina > 1): ?>
-                <li class="page-item">
-                    <a class="page-link" href="?pagina=<?= $pagina - 1 ?>" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-            <?php endif; ?>
-            <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
-                <li class="page-item <?= $i == $pagina ? 'active' : '' ?>">
-                    <a class="page-link" href="?pagina=<?= $i ?>"><?= $i ?></a>
-                </li>
-            <?php endfor; ?>
-            <?php if ($pagina < $totalPaginas): ?>
-                <li class="page-item">
-                    <a class="page-link" href="?pagina=<?= $pagina + 1 ?>" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+    
 </div>
 
         <nav aria-label="Página de navegación">
--- Sistemas ed informacion 2/config/server.php
+++ Sistemas ed informacion 2/config/server.php
@@ -1,6 +1,14 @@
 <?php
 
-	const DB_SERVER="25.31.217.253"; 
-	const DB_NAME="proyecto5";
-	const DB_USER="usuario_remoto"; 
-	const DB_PASS=''; 
+    const DB_SERVER = "localhost"; 
+    const DB_NAME = "proyecto5";
+    const DB_USER = "root"; 
+    const DB_PASS = ''; // Si tienes una contraseña para tu usuario root, colócala aquí
+
+	$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+	if (!$conn) {
+		die("Conexión fallida: " . mysqli_connect_error());
+	}
+
+?>
